8daff2fd0ab1767d981b96106b78f0b708f426a9
label <label@landteam.net>
2021-12-01 00:02:44 +0800

    build

--- docs/static/js/Sponsor427bbcb0c4b7623528bc.js
+++ docs/static/js/Sponsor427bbcb0c4b7623528bc.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{605:function(t,i,s){},902:function(t,i,s){"use strict";s(605)},909:function(t,i,s){"use strict";s.r(i);var e={name:"Sponsor"},n=(s(902),s(2)),r=Object(n.a)(e,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,i=this._self._c||t;return i("div",{staticClass:"container info"},[i("div",[this._v("祝您賺到更多BNB")]),this._v(" "),i("div",[this._v("Wish you earn more BNB")]),this._v(" "),i("div",[this._v("Address (BSC BEP20): 0x07a1e2B1580ed083f0b7d5FB024d597647579C74")])])}],!1,null,"785a5d20",null);i.default=r.exports}}]);
-//# sourceMappingURL=Sponsor40dbc34745da2ef21393.js.map
+//# sourceMappingURL=Sponsor427bbcb0c4b7623528bc.js.map
